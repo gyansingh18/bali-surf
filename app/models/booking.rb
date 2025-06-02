d048ce0f61ef1838010c6f92969d4087df7a1a78
Clint Ryan <coderclint@gmail.com>
2025-06-02 16:07:38 +0800

    Completed the creation of the databrick

---
+++ app/models/booking.rb
@@ -0,0 +1,11 @@
+class Booking < ApplicationRecord
+  belongs_to :user
+  belongs_to :surfboard
+
+  # consider adding validation for start_date and end_date should be in the future
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :total_price, presence: true
+  STATUSES = ["pending", "accepted", "rejected"]
+  validates :status, presence: true, inclusion: { in: STATUSES }
+end
