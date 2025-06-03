9f199368a3331ae0ab05337d24ea4ace553e5b86
Clint Ryan <coderclint@gmail.com>
2025-06-03 17:29:28 +0800

    Fixed migration file for category on surfboard

--- app/models/surfboard.rb
+++ app/models/surfboard.rb
@@ -1,6 +1,12 @@
 class Surfboard < ApplicationRecord
   belongs_to :user
   has_many :bookings
-  # validate tail to have set of options
-  # validate category to have set of options
+  CATEGORIES = ["Longboard", "Shortboard", "Softboard"]
+  # we've removed validates presence: true due to bug, perhaps unnessary with inclusion.
+  validates :category, inclusion: { in: CATEGORIES }
+  validates :size, presence: true
+  TAIL_TYPES = ["Square Tail", "Squash Tail", "Round Tail", "Pin Tail", "Swallow Tail"]
+  validates :tail, presence: true, inclusion: { in: TAIL_TYPES }
+  validates :location, presence: true
+  validates :price, presence: true
 end
