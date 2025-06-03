6371129c0e9d5a0cb1fbde1b118c3ec588e677a6
Jannis Braun <jannis.braun@bluewin.ch>
2025-06-03 11:50:00 +0200

    new booking v1

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,24 @@
 class BookingsController < ApplicationController
+
+  def new
+    @surfboard = Surfboard.find(params[:surfboard_id])
+    @booking = Booking.new()
+    @booking.total_price = 100
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @surfboard = Surfboard.find(params[:surfboard_id])
+    @booking.user = current_user
+    @booking.surfboard = @surfboard
+    @booking.total_price = ((@booking.end_date - @booking.start_date).to_i + 1) * 10
+    @booking.save
+    redirect_to surfboard_bookings_path
+  end
+
+  private
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :total_price)
+  end
+
 end
