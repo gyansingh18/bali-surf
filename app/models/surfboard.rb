836e0d4d2a8ae6fc0b59d4293b0430051caa1ee5
Clint Ryan <coderclint@gmail.com>
2025-06-03 11:13:33 +0800

    Added Databrick incl models for surfboard, user, booking

--- app/models/surfboard.rb
+++ app/models/surfboard.rb
@@ -1,3 +1,6 @@
 class Surfboard < ApplicationRecord
   belongs_to :user
+  has_many :bookings
+  # validate tail to have set of options
+  # validate category to have set of options
 end
--- db/migrate/20250602073445_create_surfboards.rb
+++ db/migrate/20250602073445_create_surfboards.rb
@@ -2,7 +2,7 @@ class CreateSurfboards < ActiveRecord::Migration[7.1]
   def change
     create_table :surfboards do |t|
       t.string :category, array: true
-      t.float :size, default: 0.00
+      t.float :size
       t.boolean :available, default: true
       t.string :tail
       t.references :user, null: false, foreign_key: true
