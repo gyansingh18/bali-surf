f37ac5de4533df7404639f7cb09d14d227698b1d
Clint Ryan <coderclint@gmail.com>
2025-06-06 15:06:55 +0800

    removed validation for size

--- app/models/surfboard.rb
+++ app/models/surfboard.rb
@@ -14,17 +14,17 @@ class Surfboard < ApplicationRecord
   # Constants for dropdowns/inclusions
   CATEGORIES = ["Longboard", "Shortboard", "Softboard"].freeze
   TAIL_TYPES = ["Square Tail", "Squash Tail", "Round Tail", "Pin Tail", "Swallow Tail"].freeze
-  SURFBOARD_SIZES = (5..10).each_with_object([]) do |feet, sizes|
-    (0..11).each do |inches|
-      next if feet == 5 && inches == 0
-      sizes << "#{feet}'#{inches}\""
-    end
-  end.freeze
+  # SURFBOARD_SIZES = (5..10).each_with_object([]) do |feet, sizes|
+  #   (0..11).each do |inches|
+  #     next if feet == 5 && inches == 0
+  #     sizes << "#{feet} ft. #{inches} in."
+  #   end
+  # end.freeze
 
   # Validations
   validates :category, presence: true, inclusion: { in: CATEGORIES }
-  validates :size, presence: true, inclusion: { in: SURFBOARD_SIZES }
+  validates :size, presence: true#, inclusion: { in: SURFBOARD_SIZES }
   validates :tail, presence: true, inclusion: { in: TAIL_TYPES }
   validates :location, presence: true
-  validates :price, presence: true, numericality: { greater_than: 0 } # Ensure price is a positive number
+  validates :price, presence: true, numericality: { greater_than: 0 }
 end
